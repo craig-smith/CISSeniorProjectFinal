d1d4d8350155bc747cbffad12568801a608b9a08
Craig <poland_5@msn.com>
2015-09-11 00:20:19 +0200

    Craig Smith 9/11/15 12:18am CET
    Added dynamic link functionality

---
+++ website/LinksControl.ascx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class LinksControl : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        getNavLinks();
+    }
+
+    private void getNavLinks() 
+    {
+        String username = Security.getUsername();
+
+        LinksDao dataLayer = new LinksDao();
+
+        List<Links> userLinks = dataLayer.getUserLinks(username);
+
+        foreach (Links link in userLinks)
+        {
+            writeNaveLink(link);
+        }
+    }
+
+    private void writeNaveLink(Links link)
+    {
+        String element = "<a href='" + link.getPath() + "'><div class='nav-item'>" + link.getLinkText() + "</div></a>";
+        LiteralControl userLink = new LiteralControl(element);
+
+
+        linksPlaceHolder.Controls.Add(userLink);
+    }
+}
